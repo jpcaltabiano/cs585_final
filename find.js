44cdf22b82d5b343cf2124b9c8b419e06d6c6e41
Joseph Caltabiano <jpcaltabiano@wpi.edu>
2020-12-07 17:13:10 -0500

    minor update

--- find.js
+++ find.js
@@ -10,6 +10,7 @@ async function main() {
 
   try {
     await client.connect({ useUnifiedTopology: true });
+    find_unindexed(client)
 
   } catch (e) {
     console.error(e);
@@ -19,13 +20,18 @@ async function main() {
 };
 
 
-async function find_unindexed(clinet) {
+async function find_unindexed(client) {
   let db = client.db('small');
   let collection = db.collection('small');
 
-  // drop all indexes that may exist
-  collection.dropIndexes();
-  collection.find({"firstname":"Brady"});
+  // drop all indexes that may exist, errors when no index exists
+  // collection.dropIndexes();
+
+  // collection.createIndex() // create the necessary index
+
+  //executionTimeinMillis key has time of execution, if often 0 though maybe query is just very fast
+  let res = await collection.find({"firstname":"Brady"}).explain('executionStats')
+  console.log(res);
 }
 
 main().catch(console.error);
