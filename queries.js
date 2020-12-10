81bbb75d00bcafea3afcbe65aa01b8549e0ab3ff
Joseph Caltabiano <jpcaltabiano@wpi.edu>
2020-12-10 04:18:06 -0500

    add time output to all queries

--- queries.js
+++ queries.js
@@ -11,18 +11,19 @@ async function main() {
   try {
     await client.connect({ useUnifiedTopology: true });
     let db = client.db("test");
-    let collection = db.collection("small");
-    const collections = [db.collection('small'), db.collection('medium'), db.collection('large')];
 
-    await unindexed(collection);
-    await name_index(collection);
-    await name_phone_index(collection);
-    await name_email_phone_index(collections);
-    await name_gender_index(collection);
-    await name_gender_inv_index(collection);
-    await gender_index(collections);
-    await gender_name_index(collections);
-    await gender_name_inv_index(collections);
+    console.log('\n\nTests on small database (1 million docs)\n')
+    let collection = db.collection('small');
+    await run_queries(collection);
+
+    console.log('\n\nTests on medium database (10 million docs)\n')
+    collection = db.collection('medium');
+    await run_queries(collection);
+
+    console.log('\n\nTests on large database (21 million docs)\n')
+    collection = db.collection('large');
+    await run_queries(collection);
+
 
   } catch (e) {
     console.error(e);
@@ -31,17 +32,28 @@ async function main() {
   }
 }
 
+
+async function run_queries(collection) {
+  await unindexed(collection);
+  await name_index(collection);
+  await name_phone_index(collection);
+  await name_email_phone_index(collection);
+  await name_gender_index(collection);
+  await name_gender_inv_index(collection);
+  await gender_index(collection);
+  await gender_name_index(collection);
+  await gender_name_inv_index(collection);
+}
 // does queries 1,10
 async function unindexed(collection) {
-
   //executionTimeinMillis key has time of execution, if often 0 though maybe query is just very fast
   let res = await collection
     .find({ firstname: "Brady" })
     .explain("executionStats");
   console.log(`Find name= with no index takes ${res.executionStats.executionTimeMillis} ms`);
   
-  res = await collection
-    .explain("executionStats")
+  let before = new Date();
+  await collection
     .aggregate({
       '$group': {
         '_id': {
@@ -49,13 +61,12 @@ async function unindexed(collection) {
         }
       }
     });
-  console.log(`Group by name with no index takes ${res.executionStats.executionTimeMillis} ms`);
-  // console.log(res)
+  console.log(`Group by name with no index takes ${new Date() - before} ms`);
 }
 
 // does queries 2,3,6,11,12
 async function name_index(collection) {
-  collection.createIndex({"firstname":1}) // create the necessary index
+  collection.createIndex({firstname:1}) // create the necessary index
 
   let res = await collection
     .find({ firstname: "Brady" })
@@ -72,8 +83,8 @@ async function name_index(collection) {
     .explain("executionStats");
   console.log(`Find name= and gender= with index on name takes ${res.executionStats.executionTimeMillis} ms`);
 
-  res = await collection
-    .explain("executionStats")
+  let before = new Date();
+  await collection
     .aggregate({
       '$group': {
         '_id': {
@@ -81,10 +92,10 @@ async function name_index(collection) {
         }
       }
     });
-  console.log(`Group by name with index on name takes ${res.executionStats.executionTimeMillis} ms`);
+  console.log(`Group by name with index on name takes ${new Date() - before} ms`);
   
-  res = await collection
-    .explain("executionStats")
+  before = new Date();
+  await collection
     .aggregate({
       '$group': {
         '_id': {
@@ -93,7 +104,7 @@ async function name_index(collection) {
         }
       }
     });
-  console.log(`Group by gender, name with index on name takes ${res.executionStats.executionTimeMillis} ms`);
+  console.log(`Group by gender, name with index on name takes ${new Date() - before} ms`);
   
   collection.dropIndexes();
 }
@@ -111,62 +122,50 @@ async function name_phone_index(collection) {
 }
 
 // does query 5
-async function name_email_phone_index(collections) {
-  collections.forEach(c => {
+async function name_email_phone_index(collection) {
+  collection.createIndex({firstname:1, email:1, phone:1}) // create the necessary index
   
-    c.createIndex({firstname:1, email:1, phone:1}) // create the necessary index
-    
-    let res = await c
-      .find({"firstname":"Brady", phone:"291-365-1712"})
-      .explain('executionStats')
-    console.log(`Find name= and phone= with index on name, email, and phone takes ${res.executionStats.executionTimeMillis} ms`);
-    
-    c.dropIndexes();
-  }
-  )
+  let res = await collection
+    .find({"firstname":"Brady", phone:"291-365-1712"})
+    .explain('executionStats')
+  console.log(`Find name= and phone= with index on name, email, and phone takes ${res.executionStats.executionTimeMillis} ms`);
+  
+  collection.dropIndexes();
 }
 
 // does query 7
-async function gender_index(collections) {
-  collections.forEach(c => {
+async function gender_index(collection) {
+  collection.createIndex({gender:1}) // create the necessary index
   
-    c.createIndex({gender:1}) // create the necessary index
-    
-    let res = await c
-      .find({"firstname":"Brady", gender:"M"})
-      .explain('executionStats')
-    console.log(`Find name= and gender= with index on gender takes ${res.executionStats.executionTimeMillis} ms`);
-    
-    c.dropIndexes();
-  }
-  )
+  let res = await collection
+    .find({"firstname":"Brady", gender:"M"})
+    .explain('executionStats')
+  console.log(`Find name= and gender= with index on gender takes ${res.executionStats.executionTimeMillis} ms`);
+  
+  collection.dropIndexes();
 }
 
 // does queries 9,14
-async function gender_name_index(collections) {
-  collections.forEach(c => {
+async function gender_name_index(collection) {
+  collection.createIndex({gender:1, firstname:1}) // create the necessary index
+    
+  let res = await collection
+    .find({gender:"M", "firstname":"Brady"})
+    .explain('executionStats')
+  console.log(`Find gender= and name= with index on gender and name takes ${res.executionStats.executionTimeMillis} ms`);
   
-    c.createIndex({gender:1, firstname:1}) // create the necessary index
-    
-    let res = await c
-      .find({gender:"M", "firstname":"Brady"})
-      .explain('executionStats')
-    console.log(`Find gender= and name= with index on gender and name takes ${res.executionStats.executionTimeMillis} ms`);
-    
-    res = await c
-    .explain("executionStats")
-    .aggregate({
-      '$group': {
-        '_id': {
-          'gender': '$gender',
-          'firstname': '$firstname'
-        }
+  let before = new Date();
+  await collection
+  .aggregate({
+    '$group': {
+      '_id': {
+        'gender': '$gender',
+        'firstname': '$firstname'
       }
-    });
-    console.log(`Group by gender, name with index on gender and name takes ${res.executionStats.executionTimeMillis} ms`);
-    c.dropIndexes();
-  }
-  )
+    }
+  });
+  console.log(`Group by gender, name with index on gender and name takes ${new Date() - before} ms`);
+  collection.dropIndexes();
 }
 
 // does query 8,13
@@ -178,8 +177,8 @@ async function name_gender_index(collection) {
     .explain("executionStats");
   console.log(`Find name= and phone= with index on name and gender takes ${res.executionStats.executionTimeMillis} ms`);
   
-  res = await collection
-    .explain("executionStats")
+  let before = new Date();
+  await collection
     .aggregate({
       '$group': {
         '_id': {
@@ -188,7 +187,7 @@ async function name_gender_index(collection) {
         }
       }
     });
-  console.log(`Group by gender, name with index on name and gender takes ${res.executionStats.executionTimeMillis} ms`);
+  console.log(`Group by gender, name with index on name and gender takes ${new Date() - before} ms`);
 
   collection.dropIndexes();
 }
@@ -197,7 +196,8 @@ async function name_gender_index(collection) {
 async function name_gender_inv_index(collection) {
   collection.createIndex({firstname:1, gender:-1}) // create the necessary index
 
-  let res = await collection
+  let before = new Date();
+  await collection
     .aggregate({
       '$group': {
         '_id': {
@@ -206,33 +206,28 @@ async function name_gender_inv_index(collection) {
         }
       }
     })
-    .explain("executionStats");
-  console.log(`Group by gender, name with index on name:1 and gender:-1 takes ${res.executionStats.executionTimeMillis} ms`);
+  console.log(`Group by gender, name with index on name:1 and gender:-1 takes ${new Date() - before} ms`);
 
   collection.dropIndexes();
 }
 
 // does query 16
-async function gender_name_inv_index(collections) {
-  collections.forEach(c => {
-  
-    c.createIndex({firstname:-1, gender:1}) // create the necessary index
+async function gender_name_inv_index(collection) {
+  collection.createIndex({firstname:-1, gender:1}) // create the necessary index
     
-    let res = await c
-     .aggregate({
-      '$group': {
-        '_id': {
-          'gender': '$gender',
-          'firstname': '$firstname'
-        }
+  let before = new Date();
+  await collection
+    .aggregate({
+    '$group': {
+      '_id': {
+        'gender': '$gender',
+        'firstname': '$firstname'
       }
-    })
-    .explain("executionStats");
-    console.log(`Group by gender, name with index on name:-1 and gender:1 takes ${res.executionStats.executionTimeMillis} ms`);
-    
-    c.dropIndexes();
-  }
-  )
+    }
+  })
+  console.log(`Group by gender, name with index on name:-1 and gender:1 takes ${new Date() - before} ms`);
+  
+  collection.dropIndexes();
 }
 
 main().catch(console.error);
---
+++ tester.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+
+async function main() {
+  const uri = "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb";
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect({ useUnifiedTopology: true });
+    let db = client.db("test");
+
+    let collection = db.collection('large');
+    collection.createIndex({gender:1}) // create the necessary index
+    collection.createIndex({gender:1, firstname:1}) // create the necessary index
+    collection.createIndex({firstname:1, gender:1}) // create the necessary index
+    collection.createIndex({firstname:1, gender:-1}) // create the necessary index
+    collection.createIndex({firstname:-1, gender:1}) // create the necessary index
+
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+
+
+main().catch(console.error);
